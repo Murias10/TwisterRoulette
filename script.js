40608b5b9e4a14dff43ed2e33e71d6799ba93d98
Murias10 <uo290009@uniovi.es>
2025-03-02 01:45:40 +0100

    Fix

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = 300;
 canvas.height = 300;
 
+const spanishColors = {};
+
+spanishColors["red"] = "rojo";
+spanishColors["blue"] = "azul";
+spanishColors["yellow"] = "amarillo";
+spanishColors["green"] = "verde";
+
 const colors = [
     ...Array(17).fill("red"), // 85% rojo (17 segmentos)
     "blue", "yellow", "green" // 5% cada uno
@@ -26,11 +33,17 @@ function girarRuleta() {
     rotation += randomSpin;
     canvas.style.transform = `rotate(${rotation}deg)`;
 
+    document.getElementById("boton").disabled = true;
+
     setTimeout(() => {
         const finalRotation = (rotation % 360);
         const selectedIndex = Math.floor((360 - finalRotation) / angles) % colors.length;
-        document.getElementById("resultado").innerText = `Color ganador: ${colors[selectedIndex].toUpperCase()}`;
+        document.getElementById("resultado").innerText = `Ha salido el color ${spanishColors[colors[selectedIndex]].toUpperCase()}`;
+        document.getElementById("resultado").style.color = colors[selectedIndex];
+        document.getElementById("boton").disabled = false;
     }, 3000); // Esperar a que termine de girar
+
+
 
 
 }
